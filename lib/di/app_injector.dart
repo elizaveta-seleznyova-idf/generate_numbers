1af559d62dab318f54721906111b345d46e748f8
elizaveta-seleznyova-idf <elizaveta.seleznyova@idfinance.com>
2022-08-31 15:44:33 +0300

    remove asynchrony

--- lib/di/app_injector.dart
+++ lib/di/app_injector.dart
@@ -2,7 +2,7 @@
 import 'package:domain/di/domain_injector.dart';
 import 'package:presentation/di/presentation_injector.dart';
 
-Future<void> initAppInjector() async {
+void initAppInjector()  {
   initDomainInjector();
   initPresentationInjector();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:generate_numbers/di/app_injector.dart';
 import 'package:presentation/screen/generate_numbers_app.dart';
 
-void main() async {
-  await initAppInjector();
+void main() {
+  initAppInjector();
   runApp(const GenerateNumbersApp());
 }
